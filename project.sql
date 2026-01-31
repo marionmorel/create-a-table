5c9ee6aee6f5b0af520c07fb470305a99b372b8e
marionmorel <marion.mrl@live.fr>
2026-01-31 13:41:23 +0000

    Task 4

--- project.sql
+++ project.sql
@@ -12,3 +12,10 @@ VALUES (1, 'Ororoe Munroe', '1940-05-30');
 -- Task 3
 SELECT * 
 FROM friends;
+
+-- Task 4
+INSERT INTO friends (id, name, birthday)
+VALUES (2, 'BFF One', '1997-03-11');
+
+INSERT INTO friends (id, name, birthday)
+VALUES (3, 'BFF Two', '1995-09-10');
